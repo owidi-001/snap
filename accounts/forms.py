32bd39dc5112f0eb3c0157efa503d21bf3230cbd
young <young@localhost.localdomain>
2020-12-04 05:08:35 -0500

    good entry forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,6 +9,12 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2']
+        widgets={
+            'username':forms.TextInput(attrs={'placehoder':'Username'}),
+            'email':forms.EmailInput(attrs={'placeholder': ('Email')}),
+            'password1':forms.PasswordInput(attrs={'placeholder': ('Password')}),
+            'password2':forms.PasswordInput(attrs={'placehoder':('Password Confirmation')}),
+        }
 
 class UserUpdateForm(forms.ModelForm):
     email=forms.EmailField()
